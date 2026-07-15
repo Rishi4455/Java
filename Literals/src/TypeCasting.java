5b5c5fec62bef1709582478eb1f5893379b92b67
HP <HP@DESKTOP-9L79FTN>
2026-07-15 16:38:24 +0530

    commit

---
+++ Literals/src/TypeCasting.java
@@ -0,0 +1,12 @@
+public class TypeCasting { 
+public static void main(String[] args) { 
+ 
+double d = 55.67; 
+int I = (int)d; 
+System.out.println("Double = " + d); 
+System.out.println("Integer = " + I); 
+System.out.println("Name:  Rishi Mandloi ");
+System.out.println("Enrollment: 0873CS241133 "); 
+
+} 
+}
